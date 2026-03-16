190faa8b82750e2b885b3a6733e66bcb8ebf7d3c
MohamedAntar <aboantar852003@gmail.com>
2026-03-16 16:42:09 +0200

    2-Use built-in file pipe to validate single file

--- src/files-upload/files-upload.controller.ts
+++ src/files-upload/files-upload.controller.ts
@@ -1,29 +1,34 @@
 import {
   Controller,
+  FileTypeValidator,
+  HttpStatus,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
   UploadedFile,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import {
-  FileInterceptor,
-  FilesInterceptor,
-} from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('files-upload')
 export class FilesUploadController {
   @Post('/single')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      limits: {
-        fileSize: 1024* 1024 // 1 MB
-      },
-    }),
-  )
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024, message: 'File too large' }),
+          new FileTypeValidator({ fileType: /png|jpg|jpeg/ }),
+        ],
+        errorHttpStatusCode: HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
     return file;
   }
-
 
   @Post('/multiple')
   @UseInterceptors(
@@ -34,6 +39,6 @@ export class FilesUploadController {
     }),
   )
   uploadFiles(@UploadedFiles() files: Express.Multer.File[]) {
-    return files.map(file => file.originalname);
+    return files.map((file) => file.originalname);
   }
 }
